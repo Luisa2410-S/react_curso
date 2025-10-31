85653eb4d0ca670ca493835267971e85156ebe06
Luisa2410-S <SENA@PALDFPCBIFSP221.FormacionSena.RED>
2025-10-31 09:54:21 -0500

    React

---
+++ ex_02/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <return>
+      <>
+        <h1>Hello, World!</h1>
+        <p>Welcom to my React application.</p>
+      </>
+
+
+    </return>
+  )
+}
